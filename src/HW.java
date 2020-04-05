d1cf4bed13f31f2981f1e031edadf3de0877c2a9
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-05 17:21:28 +0300

    add files

--- src/HW.java
+++ src/HW.java
@@ -1,2 +1,21 @@
 public class HW {
+    public static void main(String[] args) {
+        boolean action = true;
+        int percent;
+        if (action) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+        long amount = 1100;
+        long balance = 100;
+        long bonus = amount * percent / 100;
+        long limit = 1000;
+        long fix = 0;
+        if (amount < limit) {
+            bonus = fix;
+        }
+        long total = bonus + amount + balance;
+        System.out.println(total);
+    }
 }
